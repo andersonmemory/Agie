54e21555174a1343a1f8d5be7ddd5006ca3f96ac
andersonmemory <andersonleitedesa2100@gmail.com>
2025-03-17 21:46:21 -0300

    remove: undo member join message, don't want anymore

--- run.py
+++ run.py
@@ -6,10 +6,7 @@ import random
 
 load_dotenv()
 
-intents = discord.Intents.default()
-intents.members = True 
-
-bot = discord.Bot(intents=intents)
+bot = discord.Bot()
 
 @bot.event
 async def on_connect():
@@ -23,15 +20,5 @@ async def ping(ctx):
 async def number(ctx, inicial: int, final: int):
     value = random.randint(inicial, final)
     await ctx.respond(f"Seu número aleatório é: {value}")
-
-@bot.event
-async def on_member_join(member: discord.Member):
-    guild = member.guild
-    if (
-        guild.system_channel is not None
-    ):  # For this to work, System Messages Channel should be set in guild settings.
-        await guild.system_channel.send(f"Seja muito bem-vindo(a) {member.mention}! <:Cuteanimecatgirlpng:1254070880955404371> ")
-        await guild.system_channel.send(f"<:Keystra:1254073239877980171>")
-        await guild.system_channel.send(f"Você pode colocar uma cor no seu nick em <#1341590988996739132>. Só reagir clicando no emoji correspondente! ❤️")
 
 bot.run(os.getenv("BOT_TOKEN"))
