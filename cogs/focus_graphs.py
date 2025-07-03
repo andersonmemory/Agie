8f39e7d6bfa914249e2c39bb250c6099bdb618da
andersonmemory <andersonleitedesa2100@gmail.com>
2025-07-03 08:30:31 -0300

    feat: Graphs can be generated comparing with different users

--- cogs/focus_graphs.py
+++ cogs/focus_graphs.py
@@ -6,67 +6,85 @@ import os
 
 async def verify_additional_user_plot(ctx, bot, user : str):
     if user:
-        result = re.search(r"[0-9]+", user)
+        result = re.search(r"[0-9]+", str(user))
         if result:
             result = int(result.group(0))
             member = bot.get_user(result)
             if member:
                 new_plot = create_plot(result, bot.cursor, bot.connection)
-                return [new_plot, member]
+                if new_plot:
+                    return [new_plot, member]
+                else:
+                    await ctx.send(f"> ⚠️ : {member.global_name} ainda não tem registro de tempo de foco.", delete_after=15)
+                    return None
     
-    await ctx.send("Esse usuário não existe ou ocorreu algum outro erro.", delete_after=3)
+    await ctx.send("> ⚠️ : Esse usuário não existe ou ocorreu algum outro erro.", delete_after=15)
     return None
 
 class FocusGraphs(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        
-    @discord.slash_command(description="Gera um gráfico pessoal do membro")
-    async def grafico(self, ctx, user1 : str = None, user2 : str = None):
 
+    @discord.slash_command(description="Gera um gráfico pessoal do membro")
+    @discord.option("membro1", description="O primeiro membro para o gráfico")
+    @discord.option("membro2", description="O segundo membro para o gráfico")
+    async def grafico(self, ctx, membro1 : str = None, membro2 : str = None):
         self.connection = self.bot.connection
         self.cursor = self.bot.cursor
 
-        first_plot = create_plot(ctx.author.id, self.cursor, self.connection)
+        first_plot = create_plot(int(ctx.author.id), self.cursor, self.connection)
+
         second_plot = None
         third_plot = None
 
-        if user1:
-            second_plot = await verify_additional_user_plot(ctx, self.bot, user1)
+        if membro1:
+            second_plot = await verify_additional_user_plot(ctx, self.bot, membro1)
         
-        if user2:
-            third_plot = await verify_additional_user_plot(ctx, self.bot, user2)
+        if membro2:
+            third_plot = await verify_additional_user_plot(ctx, self.bot, membro2)
+
+        create_plot(ctx.author.id, self.cursor, self.connection)
 
         fig, ax = plt.subplots()
-        ax.plot(first_plot[0], first_plot[1], label=f"{ctx.author.global_name}")
-        ax.fill_between(first_plot[0], first_plot[1], alpha=0.15)
+
+        if first_plot:
+            ax.plot(first_plot[0], first_plot[1], label=f"{ctx.author.global_name}")
+            ax.fill_between(first_plot[0], first_plot[1], alpha=0.15)
+        else:
+            await ctx.send("> ⚠️ : você ainda não tem registro de tempo de foco.")
 
 
-        if user1:
+        if membro1 and second_plot:
             try:
                 ax.plot(second_plot[0][0], second_plot[0][1], label=f"{second_plot[1].global_name}")
                 ax.fill_between(second_plot[0][0], second_plot[0][1], alpha=0.5)
             except:
-                pass
+                print("Error")
         
-        if user2:
+        if membro2 and third_plot:
             try:
                 ax.plot(third_plot[0][0], third_plot[0][1], label=f"{third_plot[1].global_name}")
                 ax.fill_between(third_plot[0][0], third_plot[0][1], alpha=0.5)
             except:
-                pass
+                print("Error")
 
         ax.set_xlabel('Dias')
         ax.set_ylabel('Horas')
 
-        ax.set_title(f"Tempo de foco de {ctx.author.global_name}{',' if third_plot else ' e '}{f" {second_plot[1].global_name}" if second_plot else '' }{' e ' if third_plot else ''}{third_plot[1].global_name if third_plot else ''}.")
+
+        if not (second_plot or third_plot):
+            ax.set_title(f"Tempo de foco de {ctx.author.global_name}.")
+        else:
+
+            ax.set_title(f"Tempo de foco de {ctx.author.global_name}{',' if third_plot else ' e '}{f"{second_plot[1].global_name}" if second_plot else '' }{' e ' if third_plot else ''}{third_plot[1].global_name if third_plot else ''}.")
+        
         ax.legend()
         ax.grid()
 
         plt.savefig("graph.jpeg")
 
-        await ctx.send(file=discord.File('graph.jpeg'))
+        await ctx.respond(file=discord.File('graph.jpeg'))
 
         os.remove("graph.jpeg")
 
@@ -123,6 +141,9 @@ def create_plot(user_id, cursor, connection):
         connection.commit()
         
         content = cursor.fetchall()
+
+        if content[0][0] == None:
+            return None
 
         x_values = [data_point[0] for data_point in content]
         y_values = [float(data_point[1]) for data_point in content]
