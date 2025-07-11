c82bafe1cef1a2356699c4d1d933941e88b142f9
andersonmemory <andersonleitedesa2100@gmail.com>
2025-07-11 18:13:35 -0300

    feat: Add install.sh file

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "TOKEN = 'TOKEN'
+
+# The Discord bot token
+BOT_TOKEN = ''
+
+# Login to MariaDB
+# You have to fill in DB_USER and PASSWORD variables accordingly to your user.
+DB_USER = ''
+PASSWORD = ''
+HOST = '127.0.0.1'
+DATABASE = 'magil_ecosystem'
+" >> .env
