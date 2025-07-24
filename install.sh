611945829d0e37526f9cc9bc04ab2618911693db
andersonmemory <andersonleitedesa2100@gmail.com>
2025-07-24 10:04:36 -0300

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 echo "# The Discord bot token
 BOT_TOKEN = ''
 
+DEBUG_MODE = "False"
+
 # Login to MariaDB
 # You have to fill in DB_USER and PASSWORD variables accordingly to your user.
 DB_USER = ''
